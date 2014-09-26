182ab1227d50a691d8df5847d9781006cb6daf0e
Fengguo Wei <fgwei@strong-wireless-28-243.reshall.ksu.edu>
2014-09-26 12:41:36 -0500

    .gitignore works

---
+++ sireum-amandroid-alir/src/main/scala/org/sireum/amandroid/alir/dataRecorder/package.scala
@@ -0,0 +1,20 @@
+package org.sireum.amandroid.alir
+
+/** This is the package level documentation for the package 
+ * `org.my.parent.child`. Note that this documentation
+ * uses Wiki markup rather than plain vanilla HTML. This
+ * makes it:
+ *
+ * - Easier to write.
+ * 
+ * - Easier to read by a maintainer looking at the code
+ *   rather than the docs in a web browser.
+ * 
+ * - More likely to work when written to some other format.
+ *
+ * This Scaladoc comment documents both the package, and 
+ * the "package object".
+ */ 
+package object dataRecorder {
+  
+}
