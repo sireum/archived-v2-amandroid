d6e3d4a03357eff0d8570a79994fde4049ca1239
fgwei <wfg611004900521@gmail.com>
2014-09-27 12:06:08 -0500

    add copyright

--- sireum-amandroid-build/project/ProjectInfo.scala
+++ sireum-amandroid-build/project/ProjectInfo.scala
@@ -1,4 +1,11 @@
 /*
+Copyright (c) 2013-2014 Fengguo Wei & Sankardas Roy, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+/*
 Copyright (c) 2013 Robby, Kansas State University.        
 All rights reserved. This program and the accompanying materials      
 are made available under the terms of the Eclipse Public License v1.0 
