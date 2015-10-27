636b985797d1d6d2307a598b4b6a45575a8b762b
fgwei <wfg611004900521@gmail.com>
2015-10-27 03:00:05 -0400

    finish pilar dedex

---
+++ sireum-amandroid-dedex/src/main/scala/org/sireum/amandroid/dedex/FillArrayDataTask.scala
@@ -0,0 +1,76 @@
+/*
+Copyright (c) 2015-2016 Fengguo Wei, University of South Florida.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+package org.sireum.amandroid.dedex
+
+import java.io.IOException
+import org.sireum.util._
+
+/**
+ * @author fgwei
+ */
+case class FillArrayDataTask(reg: Int, target: Long, instrParser: DexInstructionToPilarParser, base: Long, offset: Long) extends PilarDedexerTask {
+  override def equals(o: PilarDedexerTask): Boolean = {
+    o match {
+      case fadt: FillArrayDataTask => super.equals(fadt)
+      case _ => false
+    }
+  }
+
+  def doTask(isSecondPass: Boolean): Unit = {
+    instrParser.placeTask(offset, this)
+  }
+
+  def renderTask(position: Long): IList[String] = {
+    val codes: MList[String] = mlistEmpty
+    val code1: StringBuilder = new StringBuilder
+    code1.append("#L%06x.  ".format(instrParser.getFilePosition))
+    val tableType = instrParser.read16Bit()
+    if(tableType != 0x300)    // type flag for array-data
+      throw new IOException("Invalid array-data table type (0x" +
+                  Integer.toHexString(tableType) +
+                  ") at offset 0x" +
+                  java.lang.Long.toHexString(instrParser.getFilePosition() - 2))
+    val bytesPerElement = instrParser.read16Bit()
+    val numberOfElements = instrParser.read32Bit()
+    code1.append("v%d:= ".format(reg))
+    code1.append("(")
+    for(l <- 0L to numberOfElements - 1L) {
+      val element = new StringBuilder()
+      val elementValue: Long = 0L
+      val byteOffset: Int = 0
+      bytesPerElement match {
+        case int if int <= 4 =>
+          var v: Int = 0 
+          for(i <- 0 to int - 1) {
+            v |= instrParser.read8Bit() << (i * 8)
+          }
+          if(l != numberOfElements - 1L) code1.append(v + ", ")
+          else code1.append(v)
+        case _ =>
+          var v: Long = 0 
+          for(i <- 0 to bytesPerElement - 1) {
+            v |= instrParser.read8Bit() << (i * 8)
+          }
+          if(l != numberOfElements - 1L) code1.append(v + ", ")
+          else code1.append(v)
+      }
+    }
+    code1.append(") @kind object;")
+    codes += code1.toString()
+    val code2: StringBuilder = new StringBuilder
+    code2.append("#L%06x.  ".format(instrParser.getFilePosition))
+    code2.append("goto L%06x;".format(target))
+    codes += code2.toString()
+    codes.toList
+  }
+
+  /**
+   * renderTask parses code
+   */
+  override def getParseFlag(position: Long): Boolean = true
+}
---
+++ sireum-amandroid-dedex/src/main/scala/org/sireum/amandroid/dedex/PilarDedexerTask.scala
@@ -0,0 +1,85 @@
+/*
+Copyright (c) 2015-2016 Fengguo Wei, University of South Florida.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+package org.sireum.amandroid.dedex
+
+/**
+ * @author fgwei
+ */
+trait PilarDedexerTask {
+  
+  def instrParser: DexInstructionToPilarParser
+  def base: Long
+  def offset: Long
+  
+  val MIN_PRIORITY = 0
+
+  /**
+   * Checks whether its parameter equals to the String content of this
+   * task. 
+   * @param The String to check for equality.
+   * @return true if the parameter is equal to the String content of this task.
+   */
+  def equals(str: String): Boolean = {
+    toString().equals(str)
+  }
+
+  /**
+   * Checks whether this DedexerTask is equals to another DedexerTask
+   * @param Another DedexerTask to check for equality
+   * @return true if this DedexerTask is equal to the other DedexerTask
+   */
+  def equals(otherTask: PilarDedexerTask): Boolean = {
+    if(otherTask == null)
+      return false
+    val otherOffset = otherTask.offset
+    val otherBase = otherTask.base
+    if((offset == 0L && base == 0L) || (otherOffset == 0L && otherBase == 0L))
+      toString().equals(otherTask.toString())
+    else
+      (offset == otherOffset) && (base == otherBase)
+  }
+
+  /**
+   * This method is specialized in child classes to execute the task.
+   * @param isSecondPass true if the second pass is executing.
+   * @throws IOException in case of file I/O error.
+   */
+  def doTask(isSecondPass: Boolean): Unit
+
+  /**
+   * This method is specialized in child classes to emit output at the 
+   * instructions's location once the instruction is reachedin the second pass.
+   * @param position Position in the code where the renderTask was called
+   */
+   def renderTask(position: Long): List[String]
+
+   override def toString: String = {
+     return getClass().getName() + "; base: 0x" +
+            java.lang.Long.toHexString(base) +
+            "; offset: 0x" +
+            java.lang.Long.toHexString(offset)
+   }
+
+  /**
+   * Retrieves the priority for this task. Tasks are executed by decreasing 
+   * priority if there are more than one tasks at a location.
+   * @return The priority of the task.
+   */
+  def getPriority: Int = MIN_PRIORITY
+
+  /**
+   * Returns a boolean flag whether the task parses code when its renderTask is
+   * invoked during the second pass. This information is used by the second
+   * pass of the disassembler: if this method returns true, the instruction parser
+   * is not invoked after the task's renderTask was invoked because it is assumed
+   * that the task itself parsed a piece of code.
+   * @param position The position in the source file.
+   * @return The parse flag for the task
+   */
+  def getParseFlag(position: Long) = false
+}
