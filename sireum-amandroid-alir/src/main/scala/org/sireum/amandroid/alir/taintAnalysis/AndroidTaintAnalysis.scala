34c78d8351ae7bc65517c18af5e4fcbe36a9d622
Fengguo Wei <fgwei@USF-Gold-Wifi-226-2-44.laptops.usf.edu>
2016-02-21 17:49:35 -0500

    modified copyright

--- sireum-amandroid-alir/src/main/scala/org/sireum/amandroid/alir/taintAnalysis/AndroidTaintAnalysis.scala
+++ sireum-amandroid-alir/src/main/scala/org/sireum/amandroid/alir/taintAnalysis/AndroidTaintAnalysis.scala
@@ -1,10 +1,19 @@
-///*
-//Copyright (c) 2013-2014 Fengguo Wei & Sankardas Roy, Kansas State University.        
-//All rights reserved. This program and the accompanying materials      
-//are made available under the terms of the Eclipse Public License v1.0 
-//which accompanies this distribution, and is available at              
-//http://www.eclipse.org/legal/epl-v10.html                             
-//*/
+/*******************************************************************************
+ * Copyright (c) 2013 - 2016 Fengguo Wei and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Main Contributors:
+ *    Fengguo Wei - Argus Lab @ University of South Florida
+ *    Sankardas Roy - Bowling Green State University
+ *    
+ * Contributors:
+ *    Robby - Santos Lab @ Kansas State University
+ *    Wu Zhou - Fireeye
+ *    Fengchi Lin - Chinese People's Public Security University
+ ******************************************************************************/
 //package org.sireum.amandroid.alir.taintAnalysis
 //
 //import org.sireum.jawa._
--- sireum-amandroid-run/src/main/scala/org/sireum/amandroid/run/framework/MalwareHiddenApiUsageStudy.scala
+++ sireum-amandroid-run/src/main/scala/org/sireum/amandroid/run/framework/MalwareHiddenApiUsageStudy.scala
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2013 - 2016 Fengguo Wei and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Main Contributors:
+ *    Fengguo Wei - Argus Lab @ University of South Florida
+ *    Sankardas Roy - Bowling Green State University
+ *    
+ * Contributors:
+ *    Robby - Santos Lab @ Kansas State University
+ *    Wu Zhou - Fireeye
+ *    Fengchi Lin - Chinese People's Public Security University
+ ******************************************************************************/
 package org.sireum.amandroid.run.framework
 
 import org.sireum.util._
